a71a723f4765573100e39cf4171bba40846f1e8b
uday kiran <udaykiran0u@gmail.com>
2025-11-12 04:59:21 +0530

    updated Remove_Nth_Node_From_End_of_List

--- LinkedList/Remove_Nth_Node_From_End_of_List.cpp
+++ LinkedList/Remove_Nth_Node_From_End_of_List.cpp
@@ -1,28 +1,3 @@
-// Given the head of a linked list, remove the nth node from the end of the list and return its head.
-
- 
-// Example 1:
-
-// Input: head = [1,2,3,4,5], n = 2
-// Output: [1,2,3,5]
-// Example 2:
-
-// Input: head = [1], n = 1
-// Output: []
-// Example 3:
-
-// Input: head = [1,2], n = 1
-// Output: [1]
- 
-
-// Constraints:
-
-// The number of nodes in the list is sz.
-// 1 <= sz <= 30
-// 0 <= Node.val <= 100
-// 1 <= n <= sz
-
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -34,6 +9,7 @@
  * };
  */
  
+ // <=================== MY SOLUTION & BEST SOLUTION ======================>
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
@@ -60,3 +36,31 @@ public:
         return head;
     }
 };
+
+
+
+
+// <============== PROBLEM DESCRIPTION ===================>
+
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+//1: Example 
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
